644c59c4e29952bbd0b9d3619f6f075cb595749f
Kody Wilson <Kody.Wilson@nordstrom.com>
2018-03-19 15:47:33 -0700

    starting the Tasks app

---
+++ Tasks/main/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", ShowAllTasksFunc)
+	http.HandleFunc("/add/", AddTaskFunc)
+	// http.HandleFunc("/add_user", PostAddUser)
+	// http.HandleFunc("/admin", HandleAdmin)
+	// http.HandleFunc("/change", PostChange)
+	// http.HandleFunc("/complete/", CompleteTaskFunc)
+	// http.HandleFunc("/completed/", ShowCompleteTasksFunc)
+	// http.HandleFunc("/delete/", DeleteTaskFunc)
+	// http.HandleFunc("/deleted/", ShowTrashTaskFunc)
+	// http.HandleFunc("/edit/", EditTaskFunc)
+	// http.HandleFunc("/login", GetLogin)
+	// http.HandleFunc("/logout", HandleLogout)
+	// http.HandleFunc("/register", PostRegister)
+	// http.HandleFunc("/restore/", RestoreTaskFunc)
+	// http.HandleFunc("/search/", SearchTaskFunc)
+	// http.HandleFunc("/trash/", TrashTaskFunc)
+	// http.HandleFunc("/update/", UpdateTaskFunc)
+
+	http.Handle("/static/", http.FileServer(http.Dir("public")))
+	log.Print("running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// AddTaskFunc show all tasks
+func AddTaskFunc(w http.ResponseWriter, r *http.Request) {
+	var message string
+	if r.Method == "GET" {
+		message = "create task GET doesn't really work"
+	} else {
+		message = "create task POST"
+	}
+	w.Write([]byte(message))
+}
+
+// ShowAllTasksFunc show all tasks
+func ShowAllTasksFunc(w http.ResponseWriter, r *http.Request) {
+	var message string
+	if r.Method == "GET" {
+		message = "all pending tasks GET"
+	} else {
+		message = "all pending tasks POST"
+	}
+	w.Write([]byte(message))
+}
